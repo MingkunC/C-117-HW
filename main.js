4d53991bb9b96f7bd32b0975ce43058152b1d8c9
Mingkun <122592808+MingkunC@users.noreply.github.com>
2023-06-08 16:39:54 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+random_number = Math.random("");
+quick_draw_data_set = []
+console.log(quick_draw_data_set[random_number]);
+timecounter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
+
+function setup()
+{
+
+}
+
+function preload()
+{
+
+}
+
+
